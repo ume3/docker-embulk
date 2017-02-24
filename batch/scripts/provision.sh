0ff26fe5ac2906122e3643ded1da08b041dc59d9
ume3 <bo2tani@gmail.com>
2017-02-24 16:16:54 +0900

    add: tsvファイルも加工前提でサンプルデータを配置してみた
    tsvとcsvを両方テストできる環境を想定しみてた
    refs:http://www.ndl.go.jp/jp/aboutus/standards/opendataset.html

--- batch/scripts/provision.sh
+++ batch/scripts/provision.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 yum install -y unzip
+
+## csv
 wget -q http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip && \
-unzip ken_all_rome.zip
+  unzip ken_all_rome.zip
+
+## tsv
+wget -q http://dl.ndl.go.jp/files/dataset/dataset_201701_t_internet.zip && \
+  unzip dataset_201701_t_internet.zip
 
 embulk gem install embulk-output-mysql
+
