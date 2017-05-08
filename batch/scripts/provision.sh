766707207a5cb8a3a90e7549d2e36871f730848e
ume3 <bo2tani@gmail.com>
2017-05-08 10:24:17 +0900

    embulk gem install embulk-output-mysql
    あらかじめmysqlプラグインを使うため

--- batch/scripts/provision.sh
+++ batch/scripts/provision.sh
@@ -10,5 +10,3 @@ wget -q http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip && \
 wget -q http://dl.ndl.go.jp/files/dataset/dataset_201701_t_internet.zip && \
   unzip dataset_201701_t_internet.zip
 
-embulk gem install embulk-output-mysql
-
